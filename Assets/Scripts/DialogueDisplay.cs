e35963d9596cd42eb87baa331af9131d8b646e11
1026932 <1026932@student.sae.edu.au>
2025-07-31 08:39:20 +1000

    Added display for text

---
+++ Assets/Scripts/DialogueDisplay.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueDisplay : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public float displaySpeed = 30f; // The speed at which the text will appear, measured in characters per second
+    public string[] segments;
+    public bool isPrinting = false; // If text is being printed to the text display right now
+
+    private bool isAwaitingInput = false; // If this script is currently waiting for an input
+    private int currentSegment = 0; // Index for the segment currently being displayed
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PrintText(segments[0]));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isAwaitingInput && Input.GetMouseButtonDown(0))
+        {
+            isAwaitingInput = false;
+            if (currentSegment < segments.Length - 1)
+            {
+                // Start displaying next segment
+                currentSegment++;
+                StartCoroutine(PrintText(segments[currentSegment]));
+            }
+            else
+            {
+                // There are no more segments to display
+                Debug.Log("DialogueDisplay has reached the end of dialogue segments");
+            }
+        }
+    }
+
+    IEnumerator PrintText(string text)
+    {
+        textDisplay.text = string.Empty;
+        isPrinting = true;
+        foreach (char c in text.ToCharArray())
+        {
+            textDisplay.text += c;
+            yield return new WaitForSeconds(1 / displaySpeed);
+        }
+        Debug.Log("Finished printing text to display");
+        isPrinting = false;
+        isAwaitingInput = true;
+    }
+}
